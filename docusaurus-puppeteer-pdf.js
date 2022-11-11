f0d4d164535cd6c79c91a48856f1df2438944532
Dan Roscigno <dan@roscigno.com>
2022-11-11 08:09:51 -0500

    Update docusaurus-puppeteer-pdf.js

--- docusaurus-puppeteer-pdf.js
+++ docusaurus-puppeteer-pdf.js
@@ -38,8 +38,6 @@ async function processLineByLine() {
     input: fileStream,
     crlfDelay: Infinity
   });
-  // Note: we use the crlfDelay option to recognize all instances of CR LF
-  // ('\r\n') in input.txt as a single line break.
 
   for await (const line of rl) {
     // Each line in input.txt will be successively available here as `line`.
